34d23550759bf8cdb5f7664627649a37dc6e3812
Chris Zuelsdorf <44318390+chriszuelsdorf@users.noreply.github.com>
2022-08-07 15:38:34 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="basetui",
+    version="0.0.1",
+    description="Bare-bones ncurses tui wrapper.",
+    url="https://github.com/chriszuelsdorf/basetui",
+    author="Chris Zuelsdorf",
+    author_email="44318390+chriszuelsdorf@users.noreply.github.com",
+    license="GNU General Public License v3 (GPLv3)",
+    packages=["basetui"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
